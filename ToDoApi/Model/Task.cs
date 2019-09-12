9c28d6b96aa8a650b267be31c439b6ebd99127a6
Dmitriuses <shpacosky.dmutro.1999.mc@gmail.com>
2019-09-12 10:02:34 +0300

    Add project files.

---
+++ ToDoApi/Model/Task.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoApi.Model
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Priority { get; set; }
+        public DateTime Deadline { get; set; }
+        public bool Status { get; set; }
+
+        public virtual Category Category { get; set; }
+        public virtual ICollection<Mark> Marks { get; set; }
+    }
+}
